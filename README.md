5315261c9b4422db519e3910f94839a0f4769cb4
lgomez17 <gomezlizbetth@gmail.com>
2022-04-20 23:00:45 -0500

    pie chart of pokemon types

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Working with CSV in JS (https://www.youtube.com/watch?v=95wAbrQAm5g)
 Installing csvtojson through npm package (https://www.npmjs.com/package/csvtojson)
 
 Installing json2csv through npm package (https://www.npmjs.com/package/json2csv)
+
+Using For Loops (https://www.w3schools.com/js/js_loop_for.asp)
--- plots.js
+++ plots.js
@@ -81,40 +81,151 @@ Plotly.newPlot("plot", traceData, layout);
 // // Render the plot to the div tag with id "plot"
 // Plotly.newPlot("plot2", traceData, layout2);
 
-// // Event handlers are just normal functions that can do anything you want
-// button.on("click", function() {
-//   d3.select(".giphy-me").html("<img src='https://gph.to/2Krfn0w' alt='giphy'>");
-// });
+// Event handlers are just normal functions that can do anything you want
+button.on("click", function() {
+  d3.select(".giphy-me").html("<img src='https://gph.to/2Krfn0w' alt='giphy'>");
+});
 
 // var data = [{
 //     values: [19, 26, 55],
 //     labels: ['Residential', 'Non-Residential', 'Utility'],
 //     type: 'pie'
 //   }];
-  
 
 
 
-//   var layout2 = {
-//     height: 400,
-//     width: 500
-//   };
-  
-//   Plotly.newPlot('plot2', data, layout2);
+// pie chart 
 
+console.log("data types", data[0].type1)
 
-var pieDiv = document.getElementById("pie-chart");
- 
-var traceA = {
-  type: "pie",
-  values: [Generation, 4916438, 4776980, 3100950, 2083210],
-  labels: ['Russia', 'Canada', 'Brazil', 'United States', 'China']
-};
- 
-var data = [traceA];
- 
+fire = 0,
+  water = 0,
+  normal = 0,
+  grass = 0,
+  electric = 0,
+  ice = 0,
+  fighting = 0,
+  poison = 0,
+  ground = 0,
+  flying = 0,
+  psychic = 0,
+  bug = 0,
+  rock = 0,
+  ghost = 0,
+  dark = 0,
+  dragon = 0,
+  steel = 0,
+  fairy = 0
+
+for (let i = 0; i < data.length; i++) {
+  if (data[i].type1 === "Fire" || data[i].type2 === "Fire") {
+    fire += 1
+  }
+}
+
+for (let i = 0; i < data.length; i++) {
+  if (data[i].type1 === "Water" || data[i].type2 === "Water") {
+    water += 1
+  }
+}
+
+for (let i = 0; i < data.length; i++) {
+  if (data[i].type1 === "Normal" || data[i].type2 === "Normal") {
+    normal += 1
+  }
+}
+for (let i = 0; i < data.length; i++) {
+  if (data[i].type1 === "Grass" || data[i].type2 === "Grass") {
+    grass += 1
+  }
+}
+for (let i = 0; i < data.length; i++) {
+  if (data[i].type1 === "Electric" || data[i].type2 === "Electric") {
+    electric += 1
+  }
+}
+for (let i = 0; i < data.length; i++) {
+  if (data[i].type1 === "Ice" || data[i].type2 === "Ice") {
+    ice += 1
+  }
+}
+for (let i = 0; i < data.length; i++) {
+  if (data[i].type1 === "Fighting" || data[i].type2 === "Fighting") {
+    fighting += 1
+  }
+}
+for (let i = 0; i < data.length; i++) {
+  if (data[i].type1 === "Poison" || data[i].type2 === "Poison") {
+    poison += 1
+  }
+}
+for (let i = 0; i < data.length; i++) {
+  if (data[i].type1 === "Ground" || data[i].type2 === "Ground") {
+    ground += 1
+  }
+}
+for (let i = 0; i < data.length; i++) {
+  if (data[i].type1 === "Flying" || data[i].type2 === "Flying") {
+    flying += 1
+  }
+}
+for (let i = 0; i < data.length; i++) {
+  if (data[i].type1 === "Psychic" || data[i].type2 === "Psychic") {
+    psychic += 1
+  }
+}
+for (let i = 0; i < data.length; i++) {
+  if (data[i].type1 === "Bug" || data[i].type2 === "Bug") {
+    bug += 1
+  }
+}
+for (let i = 0; i < data.length; i++) {
+  if (data[i].type1 === "Rock" || data[i].type2 === "Rock") {
+    rock += 1
+  }
+}
+for (let i = 0; i < data.length; i++) {
+  if (data[i].type1 === "Ghost" || data[i].type2 === "Ghost") {
+    ghost += 1
+  }
+}
+for (let i = 0; i < data.length; i++) {
+  if (data[i].type1 === "Dark" || data[i].type2 === "Dark") {
+    dark += 1
+  }
+}
+for (let i = 0; i < data.length; i++) {
+  if (data[i].type1 === "Dragon" || data[i].type2 === "Dragon") {
+    dragon += 1
+  }
+}
+for (let i = 0; i < data.length; i++) {
+  if (data[i].type1 === "Steel" || data[i].type2 === "Steel") {
+    steel += 1
+  }
+}
+for (let i = 0; i < data.length; i++) {
+  if (data[i].type1 === "Fairy" || data[i].type2 === "Fairy") {
+    fairy += 1
+  }
+}
+
+console.log("fireeee", fire)
+console.log("water", water)
+console.log("normal", normal)
+
+var data2 = [{
+  values: [normal, fire, water, grass, electric, ice, fighting, poison, ground, flying, psychic, bug, rock, ghost, dark, dragon, steel, fairy],
+  labels: ["Normal", "Fire", "Water", "Grass", "Electric", "Ice", "Fighting", "Poison", "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dark", "Dragon", "Steel", "Fairy"],
+  type: 'pie'
+}];
+
 var layout2 = {
-  title: "Area Under Forest for Different Countries"
+  height: 700,
+  width: 600,
+  margin: {"t": 25, "b": 10, "l": 10, "r": 10},
+  title: "Pokemon Types",
+  showlegend: true
 };
- 
-Plotly.plot(pieDiv, data, layout);
+
+Plotly.newPlot("plot2", data2, layout2);
